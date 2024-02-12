2ac43f766468b7120848f95aac473b23f3b9e052
Gadahun <129583121+Gadahun@users.noreply.github.com>
2024-02-12 14:40:52 +0100

    Create megoldasok2.sql

---
+++ megoldasok2.sql
@@ -0,0 +1,26 @@
+﻿-- A feladatok megoldására elkészített SQL parancsokat illessze be a feladat sorszáma után!
+
+
+-- 10. feladat:
+CREATE DATABASE halozat
+CHARACTER SET utf8
+COLLATE utf8_hungarian_ci;
+
+-- 11. feladat:
+
+
+
+-- 12. feladat:
+
+-- 13. feladat:
+
+
+-- 14. feladat:
+
+
+-- 15. feladat:
+
+
+-- 16. feladat:
+
+
